acffae53d9c27bcb728ae768f157b9dee3ae4d28
Jonas Bitoun <jonasbitoun@gmail.com>
2020-02-11 01:23:21 -0800

    Fix Build Placemarks

--- src/detection/build_placemarks.py
+++ src/detection/build_placemarks.py
@@ -42,29 +42,32 @@ class BuildPlacemarks:
                         continue
 
                     lat_long_centers = meta["predicted"][key]["coordinates"]["center"]
+                    scores = meta["predicted"][key]["score"]
 
-                    for lat_long in lat_long_centers:
-                        f.write('\t\t\t<Placemark>\n')
-                        f.write('\t\t\t\t<name>Placemark {}</name>\n'.format(i))
-                        f.write('\t\t\t\t<description>1/7/2020 2:18:10 PM</description>\n')
-                        f.write('\t\t\t\t<Style>\n')
-                        f.write('\t\t\t\t\t<LabelStyle>\n')
-                        f.write('\t\t\t\t\t\t<color>A600FFFF</color>\n')
-                        f.write('\t\t\t\t\t\t<scale>1</scale>\n')
-                        f.write('\t\t\t\t\t</LabelStyle>\n')
-                        f.write('\t\t\t\t\t<IconStyle>\n')
-                        f.write('\t\t\t\t\t\t<scale>0.5</scale>\n')
-                        f.write('\t\t\t\t\t\t<Icon>\n')
-                        f.write('\t\t\t\t\t\t\t<href>files/1.png</href>\n')
-                        f.write('\t\t\t\t\t\t</Icon>\n')
-                        f.write('\t\t\t\t\t\t<hotSpot x="0.5" y="0" xunits="fraction" yunits="fraction"/>\n')
-                        f.write('\t\t\t\t\t</IconStyle>\n')
-                        f.write('\t\t\t\t</Style>\n')
-                        f.write('\t\t\t\t<Point>\n')
-                        f.write('\t\t\t\t\t<extrude>1</extrude>\n')
-                        f.write('\t\t\t\t\t<coordinates>{},{},0 </coordinates>\n'.format(lat_long[1], lat_long[0]))
-                        f.write('\t\t\t\t</Point>\n')
-                        f.write('\t\t\t</Placemark>\n')
+                    for k in range(len(lat_long_centers)):
+                        lat_long = lat_long_centers[k]
+                        if scores[k] > self.threshold:
+                            f.write('\t\t\t<Placemark>\n')
+                            f.write('\t\t\t\t<name>Placemark {}</name>\n'.format(i))
+                            f.write('\t\t\t\t<description>1/7/2020 2:18:10 PM</description>\n')
+                            f.write('\t\t\t\t<Style>\n')
+                            f.write('\t\t\t\t\t<LabelStyle>\n')
+                            f.write('\t\t\t\t\t\t<color>A600FFFF</color>\n')
+                            f.write('\t\t\t\t\t\t<scale>1</scale>\n')
+                            f.write('\t\t\t\t\t</LabelStyle>\n')
+                            f.write('\t\t\t\t\t<IconStyle>\n')
+                            f.write('\t\t\t\t\t\t<scale>0.5</scale>\n')
+                            f.write('\t\t\t\t\t\t<Icon>\n')
+                            f.write('\t\t\t\t\t\t\t<href>files/1.png</href>\n')
+                            f.write('\t\t\t\t\t\t</Icon>\n')
+                            f.write('\t\t\t\t\t\t<hotSpot x="0.5" y="0" xunits="fraction" yunits="fraction"/>\n')
+                            f.write('\t\t\t\t\t</IconStyle>\n')
+                            f.write('\t\t\t\t</Style>\n')
+                            f.write('\t\t\t\t<Point>\n')
+                            f.write('\t\t\t\t\t<extrude>1</extrude>\n')
+                            f.write('\t\t\t\t\t<coordinates>{},{},0 </coordinates>\n'.format(lat_long[1], lat_long[0]))
+                            f.write('\t\t\t\t</Point>\n')
+                            f.write('\t\t\t</Placemark>\n')
 
                         i += 1
 
@@ -77,9 +80,9 @@ class BuildPlacemarks:
 
 if __name__ == "__main__":
 
-    meta_folder = "C:\\Users\\jonas\\Desktop\\Detection\\Detection_Dataset_meta"
+    meta_folder = "C:\\Users\\AISG\\Documents\\Jonas\\Real_World_Dataset_TMS_meta\\"
     model_number = 7
-    threshold = 0.95
+    threshold = 0.9999
 
     build_placemarks = BuildPlacemarks(meta_folder,
                                        model_number,
