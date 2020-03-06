dabac68d9d151cce2c58844c1071309faab349a4
Jonas Bitoun <jonasbitoun@gmail.com>
2020-03-06 11:03:19 +0800

    Predict from Index

--- src/knn/knn_predict.py
+++ src/knn/knn_predict.py
@@ -11,19 +11,25 @@ from knn_training import KNNTraining
 
 class KNNPredict:
 
-    def __init__(self, image_folder, meta_folder, model_number, knn_weights_filename,
+    def __init__(self, image_folder, meta_folder, index_path_filename, model_number, knn_weights_filename,
                  mode="histogram", size=(64, 64), bins=(8, 8, 8)):
 
         self.image_folder = image_folder
         self.meta_folder = meta_folder
+        self.index_path_filename = index_path_filename
         self.model_number = model_number
         self.mode = mode
         self.knn = joblib.load(knn_weights_filename)
 
-        self.knn_build_database = KNNBuildDatabase(self.image_folder, self.meta_folder, self.model_number, train=False, TMS=True)
-        self.knn_build_database.run()
-        self.image_id = self.knn_build_database.image_id
-        self.X = self.knn_build_database.X
+        self.X = []
+        self.image_id = []
+
+        # self.knn_build_database = KNNBuildDatabase(self.image_folder, self.meta_folder, self.model_number, train=False, TMS=True)
+        # self.knn_build_database.run()
+        # self.image_id = self.knn_build_database.image_id
+        # self.X = self.knn_build_database.X
+        self.load_image_to_predict()
+
         print(len(self.X))
 
         if mode == "raw_pixel":
@@ -34,6 +40,30 @@ class KNNPredict:
             raise ValueError("mode is \'raw_pixel\' or \'histogram\'")
 
         print(self.features.shape)
+
+    def load_image_to_predict(self):
+        with open(self.index_path_filename, 'r') as f:
+            for line in f:
+                meta_filename = line
+                meta_info = os.path.splitext(meta_filename)[0].split('_')
+                zoom = meta_info[1]
+                xtile = meta_info[2]
+                ytile = meta_info[3]
+                image_path = os.path.join(self.image_folder, zoom, xtile, str(ytile)+".jpg")
+                meta_path = os.path.join(self.meta_folder, zoom, meta_filename)
+                image = cv2.imread(image_path)
+                with open(meta_path, 'r') as f_meta:
+                    meta = json.load(f_meta)
+                predicted = meta["predicted"][f'model_{self.model_number}']
+                bboxes_predicted = predicted["box"]
+                for box_predicted in bboxes_predicted:
+                    x_min = box_predicted[0]
+                    y_min = box_predicted[1]
+                    x_max = box_predicted[2]
+                    y_max = box_predicted[3]
+                    image_box = image[x_min:x_max, y_min:y_max, :]
+                    self.X.append(image_box)
+                    self.image_id.append(os.path.splitext(meta_filename)[0])
 
     def predict(self):
         self.y_predict = self.knn.predict(self.features)
@@ -70,6 +100,7 @@ if __name__ == "__main__":
 
     image_folder = "C:\\Users\\AISG\\Documents\\Jonas\\Real_World_Dataset_TMS\\sat"
     meta_folder = "C:\\Users\\AISG\\Documents\\Jonas\\Real_World_Dataset_TMS_meta\\sat"
+    index_path_score_filename = "C:\\Users\\AISG\\Documents\\Jonas\\helipad_detection\\src\\database_management\\helipad_path_over_0.999.txt"
     model_number = 7
     knn_weights_filename = "knn_histogram_2.pkl"
 
